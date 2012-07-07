9b2e0e9125a5efcb38a542c6b0df23b5c39b0835
James Fickel <jamesfickel@gmail.com>
2012-07-07 13:13:42 -0500

    Add user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,12 +2,17 @@ FactoryGirl.define do
 	
 	factory :user do
 		sequence(:name)  { |n| "Person #{n}" }
-    sequence(:email) { |n| "person_#{n}@example.com"}
-    password "foobar"
-    password_confirmation "foobar"
+        sequence(:email) { |n| "person_#{n}@example.com"}
+        password "foobar"
+        password_confirmation "foobar"
 
-    factory :admin do
-      admin true
-    end
+        factory :admin do
+          admin true
+        end
 	end
+
+    factory :micropost do
+        content "Lorem ipsum"
+        user
+    end
 end
